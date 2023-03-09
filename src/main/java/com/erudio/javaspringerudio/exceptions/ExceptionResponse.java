6f915136b706fbf174d7d244b03efcb05703fd5e
pedrohenmonteiro <pedromont436@gmail.com>
2023-03-09 15:57:45 -0300

    Exception handler

--- src/main/java/com/erudio/javaspringerudio/exceptions/ExceptionResponse.java
+++ src/main/java/com/erudio/javaspringerudio/exceptions/ExceptionResponse.java
@@ -1,8 +1,11 @@
 package com.erudio.javaspringerudio.exceptions;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class ExceptionResponse {
+public class ExceptionResponse implements Serializable {
+
+  private static final long serialVersionUID = 1L;
   private Date timestamp;
   private String message;
   private String details;
@@ -13,30 +16,16 @@ public class ExceptionResponse {
     this.details = details;
   }
 
-  public ExceptionResponse() {
-  }
-
   public Date getTimestamp() {
     return this.timestamp;
-  }
-
-  public void setTimestamp(Date timestamp) {
-    this.timestamp = timestamp;
   }
 
   public String getMessage() {
     return this.message;
   }
 
-  public void setMessage(String message) {
-    this.message = message;
-  }
-
   public String getDetails() {
     return this.details;
   }
 
-  public void setDetails(String details) {
-    this.details = details;
-  }
 }
---
+++ src/main/java/com/erudio/javaspringerudio/exceptions/handler/CustomizedResponseEntityException.java
@@ -0,0 +1,25 @@
+package com.erudio.javaspringerudio.exceptions.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.erudio.javaspringerudio.exceptions.ExceptionResponse;
+
+@RestController
+@ControllerAdvice
+public class CustomizedResponseEntityException extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(Exception.class)
+  public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest req) {
+    var exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), req.getDescription(false));
+    return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+}
