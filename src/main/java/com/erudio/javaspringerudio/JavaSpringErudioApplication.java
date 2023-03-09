27fbd23a1b2d636a3a28d8c234c8686c6dfb07c4
pedrohenmonteiro <pedromont436@gmail.com>
2023-03-09 11:26:09 -0300

    first commit

---
+++ src/main/java/com/erudio/javaspringerudio/JavaSpringErudioApplication.java
@@ -0,0 +1,13 @@
+package com.erudio.javaspringerudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringErudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erudio/javaspringerudio/JavaSpringErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erudio.javaspringerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
