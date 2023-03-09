2d3dfb2a34eee1a4771519978b1981598dd01b39
pedrohenmonteiro <pedromont436@gmail.com>
2023-03-09 15:47:31 -0300

    Exception Response class

---
+++ src/main/java/com/erudio/javaspringerudio/exceptions/ExceptionResponse.java
@@ -0,0 +1,42 @@
+package com.erudio.javaspringerudio.exceptions;
+
+import java.util.Date;
+
+public class ExceptionResponse {
+  private Date timestamp;
+  private String message;
+  private String details;
+
+  public ExceptionResponse(Date timestamp, String message, String details) {
+    this.timestamp = timestamp;
+    this.message = message;
+    this.details = details;
+  }
+
+  public ExceptionResponse() {
+  }
+
+  public Date getTimestamp() {
+    return this.timestamp;
+  }
+
+  public void setTimestamp(Date timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public String getMessage() {
+    return this.message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getDetails() {
+    return this.details;
+  }
+
+  public void setDetails(String details) {
+    this.details = details;
+  }
+}
